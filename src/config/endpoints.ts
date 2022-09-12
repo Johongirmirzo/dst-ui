8f58a99961e6c5e5f4d0ebd6fa1ee600a7d67da8
Johongirr <johongir_rahimov@bk.ru>
2022-09-12 08:55:53 +0200

    fixed uri issue

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -7,10 +7,11 @@ const ENDPOINTS = {
     REGISTER_USER: "/auth/register",
     LOGIN_USER: "/auth/login",
     LOGOUT_USER: "/auth/logout",  
-    USER_TOKEN_URL: "/auth/login/success",
-    FACEBOOK_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/facebook",
-    GOOGLE_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/google",
-    LINKEDIN_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/linkedin"
+    
+    // USER_TOKEN_URL: "/auth/login/success",
+    // FACEBOOK_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/facebook",
+    // GOOGLE_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/google",
+    // LINKEDIN_AUTH_URL: "https://daily-sleep-trackker.herokuapp.com/auth/linkedin"
 }
 
 export default ENDPOINTS
