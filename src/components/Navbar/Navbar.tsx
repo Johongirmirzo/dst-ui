62f629566be29095af1840e335b6cc3bf3aa48e2
Johongirr <johongir_rahimov@bk.ru>
2022-09-21 07:32:48 +0200

    Set correct form width

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -26,6 +26,7 @@ const Navbar = () => {
       display="flex"
       justifyContent="space-between"
       alignItems="center"
+      flexDirection={{ base: "column", sm: "row" }}
     >
       <Heading fontSize={{ base: "20", sm: "25", md: "30" }}>
         <Link to="/">Daily Sleep Tracker</Link>
