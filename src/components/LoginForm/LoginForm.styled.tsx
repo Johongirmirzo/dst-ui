2b576bab49db88ac0682353f4b885aa65b227030
Johongirr <johongir_rahimov@bk.ru>
2022-09-21 07:23:13 +0200

    Set correct form width

--- src/components/LoginForm/LoginForm.styled.tsx
+++ src/components/LoginForm/LoginForm.styled.tsx
@@ -25,8 +25,8 @@ const LoginBox = styled.div`
   justify-content: center;
 `;
 const LoginFormBox = styled.section`
-  min-width: 350px;
-  max-width: 450px;
+  min-width: 450px;
+  max-width: 500px;
   background: #f7f7f7;
   border-radius: 5px;
   padding: 25px;
